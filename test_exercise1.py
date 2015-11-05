535fb367542230b12b28e71b9e8fe2ce0054d4ce
aacampb <aaronl.campbell@mail.utoronto.ca>
2015-11-04 19:13:06 -0500

    Commit

--- test_exercise1.py
+++ test_exercise1.py
@@ -1,9 +1,26 @@
+#!/usr/bin/env python
 
-VOWELS = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
-statement = raw_input("Please enter a word: ")
-words =  statement.split()
-for word in words:
-    if word[0] in VOWELS:
-        print word + "ay",
-    else:
-        print word[1:] + word[0] + "ay",
+""" Assignment 2, Exercise 1, INF1340, Fall, 2015. Pig Latin
+
+Test module for exercise1.py
+
+"""
+
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+__copyright__ = "2015 Susan Sim"
+__license__ = "MIT License"
+
+
+from exercise1 import pig_latinify
+
+
+def test_basic():
+    """
+    Basic test cases from assignment hand out
+    """
+    assert pig_latinify("dog") == "ogday"
+    assert pig_latinify("scratch") == "atchscray"
+    assert pig_latinify("is") == "isyay"
+    assert pig_latinify("apple") == "appleyay"
+
