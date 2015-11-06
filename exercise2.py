74f82108eee68c18e1d2bcaa785adc889ffaf685
SebsMacBook <sebastien.dagenais.maniatopoulos@mail.utoronto.ca>
2015-11-05 23:31:25 -0500

    Brain still hurts!  Here come the test cases

--- exercise2.py
+++ exercise2.py
@@ -46,9 +46,18 @@ def find(input_string, substring, start, end):
 def multi_find(input_string, substring, start, end):
     """
     Describe your function
-    :param :
-    :return:
-    :raises:
+    :param : we are essentially using the same formula as the our find function by using the find function
+            in our tempResult function
+     substring - What we are looking for in input_string
+			 start - Start index for our search
+			 end - End index for our search
+			 count - tracks the amount of matching characters found in a row and keeps tracks of which character we are looking for
+			 position - used as an indicator of which index we are currently at in our input_string
+
+    :return: Function returns the starting index of the substring that has been located in the input_string.
+    If the substring is not found: simply return -1
+    If the substring is found, tempResult will continue until the next match is found until end.
+    :raises: No exceptions will be raised
     """
 
     position = start
@@ -65,7 +74,4 @@ def multi_find(input_string, substring, start, end):
         result += str(tempResult)
 
     return result
-
-assert find("This is an ex-parrot", "parrot", 0, 20) ==14
-assert multi_find("Ni! Ni! Ni! Ni!", "Ni", 0, 15)
 
